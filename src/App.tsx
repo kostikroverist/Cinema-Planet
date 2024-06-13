f59a684877c885da6d5d128970d47369c17440c0
kostikroverist <kostikzhora@gmail.com>
2024-06-13 19:41:16 +0300

    initial-project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="flex items-center justify-center min-h-screen bg-gray-100">
+        <h1 className="text-4xl font-bold text-blue-600">
+          Hello, Tailwind CSS!
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
