060c38f7641c443518de928b11c11688aee513bc
kostikroverist <kostikzhora@gmail.com>
2024-06-13 19:45:48 +0300

    deploy

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ function App() {
     <>
       <div className="flex items-center justify-center min-h-screen bg-gray-100">
         <h1 className="text-4xl font-bold text-blue-600">
-          Hello, Tailwind CSS!
+          Hello, Tailwind CSS!and Deploy Project
         </h1>
       </div>
     </>
